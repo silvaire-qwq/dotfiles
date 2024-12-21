18999494ae0ae88d1e72de0ab4faac7478594c57
silvaire-qwq <silvaire_qwq@outlook.com>
2024-12-21 21:18:37 +0800

    Updated by push.sh

--- deps/push.sh
+++ deps/push.sh
@@ -1,5 +1,47 @@
 #!/usr/bin/bash
 
-sudo git add .
-sudo git commit -m "Updated by push.sh"
-sudo git push -uf origin main
+function git-three(){
+    sudo git add .
+    case $? in
+        "0")
+            sleep 0
+            ;;
+        *)
+            echo -e "\e[31m==>\e[0m 运行失败。"
+            exit 1
+            ;;
+    esac
+    sudo git commit -m "Updated by push.sh"
+    case $? in
+        "0")
+            sleep 0
+            ;;
+        *)
+            echo -e "\e[31m==>\e[0m 运行失败。"
+            exit 1
+            ;;
+    esac
+    sudo git push -uf origin main
+    case $? in
+        "0")
+            sleep 0
+            ;;
+        *)
+            echo -e "\e[31m==>\e[0m 运行失败。"
+            exit 1
+            ;;
+    esac
+}
+
+echo -e "\e[34m==>\e[0m 尝试推送至 Git 远程仓库"
+git-three
+case $? in
+        "0")
+            echo -e "\e[32m==>\e[0m 成功。"
+            ;;
+        *)
+            echo -e "\e[31m==>\e[0m 运行失败。"
+            exit 1
+            ;;
+    esac
+
--- deps/update.sh
+++ deps/update.sh
@@ -2,21 +2,21 @@
 
 case $(whoami) in
 	"root")
-		echo -e "\e[31m*\e[0m 你不能使用 Root 账户运行这个脚本。"
+		echo -e "\e[31m==>\e[0m 你不能使用 Root 账户运行这个脚本。"
 		exit 1
 		;;
 esac
 
 if test -d $ScriptDir; then
 	for profile in "$ScriptDir"/*.sh; do
-		echo -e "\e[34m*\e[0m 尝试运行 $profile"
 		test -r "$profile" && . "$profile"
 		case $? in
 			"0")
-				echo -e "\e[32m*\e[0m 成功。"
+				sleep 0
 				;;
 			*)
-				echo -e "\e[31m*\e[0m 失败。"
+				echo -e "\e[31m==>\e[0m 运行 $profile 时失败。"
+				exit 1
 				;;
 		esac
 	done
