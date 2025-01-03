fe8b7240c3f3594b64f2f1b18ea082c9e359d6d3
silvaire-qwq <silvaire_qwq@outlook.com>
2025-01-03 13:30:44 +0800

    [AP] Boot On 2025/01/03 13:30:44

--- home/.startup.sh
+++ home/.startup.sh
@@ -1,5 +1,15 @@
 # Clear Screen
 clear
 
+# Auto Check & Update Dotfiles
+cd ~/Dotfiles
+
+if [[ $(sudo git diff | wc -l) -ne 0 ]]; then
+	# Need Update
+	sudo git add .
+	sudo git commit -m "[AP] Boot On $(date "+%Y/%m/%d %H:%M:%S")"
+	sudo git push
+fi
+
 # Start Hyprland
 Hyprland >/dev/null 2>&1
