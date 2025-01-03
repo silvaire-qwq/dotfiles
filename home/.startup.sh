95848c7792313ec56d540f0044ab005e14d2adfc
silvaire-qwq <silvaire_qwq@outlook.com>
2025-01-03 13:54:29 +0800

    * Started On 2025/01/03 13:54:29

--- home/.startup.sh
+++ home/.startup.sh
@@ -16,6 +16,7 @@ if [[ $(sudo git diff | wc -l) -ne 0 ]]; then
 	sudo git commit -m "* Started On $(date "+%Y/%m/%d %H:%M:%S")"
 	# Remote
 	sudo git push -uf origin main
+	# Stop Grey
 	echo -e "\e[0m"
 	# Language
 	export LANG="zh_CN.UTF-8"
