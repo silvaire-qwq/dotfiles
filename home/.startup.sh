7a3dd964360f70652bac94dde58903675eecd37c
silvaire-qwq <silvaire_qwq@outlook.com>
2025-01-03 13:53:09 +0800

    * Started On 2025/01/03 13:53:09

--- home/.startup.sh
+++ home/.startup.sh
@@ -7,13 +7,14 @@ cd ~/Dotfiles
 if [[ $(sudo git diff | wc -l) -ne 0 ]]; then
 	# Language
 	export LANG="en_US.UTF-8"
-	# Add Files
-	sudo git add . &>/dev/null
-	# Commit
-	sudo git commit -m "* Started On $(date "+%Y/%m/%d %H:%M:%S")" &>/dev/null
-	# Remote
-	echo -e "\e[35;1m==>\e[0;1m Ready to Push"
+	# Prompt
+	echo -e "\e[34m*\e[0m Ready to Push"
 	echo -e "\e[90;2m"
+	# Add Files
+	sudo git add .
+	# Commit
+	sudo git commit -m "* Started On $(date "+%Y/%m/%d %H:%M:%S")"
+	# Remote
 	sudo git push -uf origin main
 	echo -e "\e[0m"
 	# Language
