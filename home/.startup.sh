1b7c86555748e54f6fa753ed9044b57d82307c5e
silvaire-qwq <silvaire_qwq@outlook.com>
2025-01-03 13:49:31 +0800

    * Started On 2025/01/03 13:49:31

--- home/.startup.sh
+++ home/.startup.sh
@@ -5,13 +5,19 @@ clear
 cd ~/Dotfiles
 
 if [[ $(sudo git diff | wc -l) -ne 0 ]]; then
+	# Language
+	export LANG="en_US.UTF-8"
 	# Add Files
 	sudo git add . &>/dev/null
 	# Commit
 	sudo git commit -m "* Started On $(date "+%Y/%m/%d %H:%M:%S")" &>/dev/null
 	# Remote
-	export LANG="en_US.UTF-8"
+	echo -e "\e[35;1m==>\e[0;1m Ready to Push"
+	echo -e "\e[90;2m"
 	sudo git push -uf origin main
+	echo -e "\e[0m"
+	# Language
+	export LANG="zh_CN.UTF-8"
 fi
 
 # Start Hyprland
