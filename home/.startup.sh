a10316053674dcc35d1b7194a5b360433eb7b2f5
silvaire-qwq <silvaire_qwq@outlook.com>
2025-02-04 08:24:34 +0800

    Auto

--- home/.startup.sh
+++ home/.startup.sh
@@ -1,12 +1,5 @@
 # Clear Screen
 clear
 
-# Check Update Dotfiles
-cd ~/Dotfiles
-if [[ $(sudo git diff | wc -l) -ne 0 ]]; then
-	sudo git add .
-	sudo git commit -m "Local Commit on Boot"
-fi
-
 # Start Hyprland
 Hyprland &>/dev/null
