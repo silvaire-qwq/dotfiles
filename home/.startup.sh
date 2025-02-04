07ba2ba7a901b9db44d3c32a8b87557ad4a9da3d
silvaire-qwq <silvaire_qwq@outlook.com>
2025-02-04 08:21:19 +0800

    Local Push on Boot

--- home/.startup.sh
+++ home/.startup.sh
@@ -1,5 +1,13 @@
 # Clear Screen
 clear
 
+# Check Update Dotfiles
+cd ~/Dotfiles
+if [[ $(sudo git diff | wc -l) -ne 0 ]]; then
+	sudo git add .
+	sudo git commit -m "Local Push on Boot"
+	sudo git push
+fi
+
 # Start Hyprland
 Hyprland &>/dev/null
